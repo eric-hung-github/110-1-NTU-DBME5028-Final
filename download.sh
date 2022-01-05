6ea704e58688a99a30c613b35283ca79b51bfb28
YunHisangTang <47920363+YunHisangTang@users.noreply.github.com>
2022-01-05 08:37:57 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
 
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mliRlkDKqMbjzUyg1q9e_yw7ptkRQmxf' -O model_state.pth.tar
+gdrive_download(){
+CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+rm -rf /tmp/cookies.txt
+}
+
+gdrive_download '1JETBf090WcdQ6sxbdaq57Nx5FRQ9BF93' model_state.pth.tar
